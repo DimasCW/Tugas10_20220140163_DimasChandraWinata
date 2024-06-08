7cc677ca7bd2e80593262a96b73d64db0cc67d6d
DimasCW <cdimas292@gmail.com>
2024-06-08 21:15:49 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        First Project Reaact Js
+      </h1>
+      <p>Ini adalah project react js pertama di matakuliah pengembangan desain web</p>
+      <p>Dimas Chandra Winata</p>
+      <p>20220140163</p>
+      <img src='https://www.patterns.dev/img/reactjs/react-logo@3x.svg' />
     </div>
   );
 }
-
+``
 export default App;
